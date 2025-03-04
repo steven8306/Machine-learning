9c8fa71c7fab85f6d92a12dc5f18ae3da712fc56
steven8306 <stevenjohn201315@gmail.com>
2025-03-03 22:19:31 -0800

    add validation with split method

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
 iowa_file_path = './melb_data.csv'
 home_data=pd.read_csv(iowa_file_path)
 home_data = home_data.dropna(axis=0)
 y=home_data.Price
-features=['Rooms','Bathroom','Car','Regionname','Longtitude']
+features=['Rooms','Bathroom','Landsize','Lattitude','Longtitude']
 X=home_data[features]
 
+#define model
+from sklearn.tree import DecisionTreeRegressor
 home_model=DecisionTreeRegressor(random_state=1)
-# home_model.fit(X, y)
-print(X.describe())
+home_model.fit(X, y)
+print(home_data.head())
+print("result of model")
+print(home_model.predict(X.head()))
 
-# print("result of model")
-# print(home_model.predict(X.head()))
+#model validation
+from sklearn.metrics import mean_absolute_error
+predicted_home_prices=home_model.predict(X)
+mean_absolute_error(y,predicted_home_prices)
+print(mean_absolute_error(y,predicted_home_prices))
+
+#test-split validation
+from sklearn.model_selection import train_test_split
+train_x, val_x, train_y, val_y = train_test_split(X,y,random_state=0)
+#define model
+improved_model = DecisionTreeRegressor()
+#fit model
+improved_model.fit(train_x,train_y)
+val_prediction = improved_model.predict(val_x)
+print(mean_absolute_error(val_y,val_prediction))
