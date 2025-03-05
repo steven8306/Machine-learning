2f167ee7fe6e5fb494b95102fe3074509081f441
steven8306 <stevenjohn201315@gmail.com>
2025-03-05 02:07:48 -0800

    complete

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ X=home_data[features]
 from sklearn.tree import DecisionTreeRegressor
 home_model=DecisionTreeRegressor(random_state=1)
 home_model.fit(X, y)
-print(home_data.head())
+print(home_data.groupby('Landsize').Landsize.count())
 print("result of model")
-print(home_model.predict(X.head()))
+#print(home_model.predict(X.head()))
 
 #model validation
 from sklearn.metrics import mean_absolute_error
 predicted_home_prices=home_model.predict(X)
 mean_absolute_error(y,predicted_home_prices)
-print(mean_absolute_error(y,predicted_home_prices))
+#print(mean_absolute_error(y,predicted_home_prices))
 
 #test-split validation
 from sklearn.model_selection import train_test_split
@@ -28,4 +28,14 @@ improved_model = DecisionTreeRegressor()
 #fit model
 improved_model.fit(train_x,train_y)
 val_prediction = improved_model.predict(val_x)
-print(mean_absolute_error(val_y,val_prediction))
+#print(mean_absolute_error(val_y,val_prediction))
+#randomforest model
+from sklearn.ensemble import RandomForestRegressor
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_x, train_y)
+melb_preds = forest_model.predict(val_x)
+print(mean_absolute_error(val_y, melb_preds))
+
+output = pd.DataFrame({'Id': val_x.index,
+                       'SalePrice': melb_preds})
+output.to_csv('submission.csv', index=False)
